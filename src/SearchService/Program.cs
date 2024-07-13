67ae4f9026d21e0c468990175a485143ee96a939
Afaz <afaz.munshi49@gmail.com>
2024-07-13 14:27:29 -0500

    Created Search Service which gets all its search data from the Auction Service

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,40 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Log the current environment
+Console.WriteLine($"Environment: {builder.Environment.EnvironmentName}");
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+// Configure the Http request pipeline
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
